2b04df021068ca7a907a875c8b68d7dbea02c1cf
Mikko Drocan <mikko.drocan@iki.fi>
2023-09-09 11:10:45 +0300

    output more readable

--- fronius.py
+++ fronius.py
@@ -3,6 +3,7 @@ Importing data from the Fronius device.
 """
 import requests  # pylint: disable=import-error
 from requests.exceptions import HTTPError  # pylint: disable=import-error
+from numerize import numerize  # pylint: disable=import-error
 
 with open("api.txt", "r", encoding="utf8") as file:
     URL = file.read().rstrip()
@@ -20,16 +21,23 @@ def check_values():
         jsondata = response.json()
 
         print("Timestamp:", jsondata["Head"]["Timestamp"])
-        print("Currently:", jsondata["Body"]["Data"]["Site"]["P_PV"])
-        print("Day:", jsondata["Body"]["Data"]["Site"]["E_Day"])
-        print("Year:", jsondata["Body"]["Data"]["Site"]["E_Year"])
-        print("Total:", jsondata["Body"]["Data"]["Site"]["E_Total"])
+
+        currently = numerize.numerize(jsondata["Body"]["Data"]["Site"]["P_PV"], 2)
+        print("Currently:", currently,"Wh")
+
+        today = numerize.numerize(jsondata["Body"]["Data"]["Site"]["E_Day"], 2)
+        print("Today:", today,"Wh")
+
+        year = numerize.numerize(jsondata["Body"]["Data"]["Site"]["E_Year"], 2)
+        print("Year:", year,"Wh")
+
+        total_produce = numerize.numerize(jsondata["Body"]["Data"]["Site"]["E_Total"], 2)
+        print("Total:", total_produce,"Wh")
 
     except HTTPError as http_err:
         print(f"HTTP error occurred: {http_err}")
     except Exception as err:  # pylint: disable=broad-except
         print(f"Other error occurred: {err}")
-
 
 if __name__ == "__main__":
     check_values()
